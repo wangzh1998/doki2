4c53c9bf6faa8150d00062a9de0ba3ae42313f41
wangzh <1759630417@qq.com>
2020-06-16 11:10:48 +0800

    更新controller

--- src/main/java/com/androidproj/doki2/controller/UserController.java
+++ src/main/java/com/androidproj/doki2/controller/UserController.java
@@ -1,30 +1,51 @@
 package com.androidproj.doki2.controller;
 
+import com.androidproj.doki2.entity.Saying;
 import com.androidproj.doki2.entity.User;
+import com.androidproj.doki2.service.SayingService;
 import com.androidproj.doki2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     static int idx = 0;
 
     @Autowired
     UserService userService;
+    @Autowired
+    SayingService sayingService;
+
+//    @RequestMapping("/personal")
+//    public User getUser(){
+//
+//    }
+//    @RequestMapping("/mySendingSayings")
+//    public List<Saying> getMySendingSayings(){
+//
+//    }
+//    @RequestMapping("/myGettingSayings")
+//    public List<Saying> getMyGettingSayings(){
+//
+//    }
+
+
 
     @RequestMapping("/users")
     public List<User> getUsers(){
         return userService.getAllUsers();
     }
 
-    @RequestMapping("/createUser")
+    @RequestMapping("/register")
     public String createUsers() throws Exception {
         User user = new User();
         user.setGender(0);
-        user.setNickName("nick");
+        user.setNickName("nick"+idx);
         user.setRealName("real");
         user.setPassword("1234");
         user.setPhoneNumber("1234567890"+idx);
